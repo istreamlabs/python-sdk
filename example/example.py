997bbbd8e7d01acd09dd0138af41a0db8ec58b5a
shaunflagg <sflagg@istreamplanet.com>
2022-03-25 15:26:55 -0400

    readme cleanup and typo fix in examplepy

--- example/example.py
+++ example/example.py
@@ -1,7 +1,7 @@
 from openapi_client.helper import *
 import sys, os
 
-# add the organization you would like to view e.g. istreamplant-internal
+# add the organization you would like to view e.g. istreamplanet-internal
 ORGANIZATION = "ORGANIZATION"
 
 access_token = get_access_token(os.getenv('ISP_CLIENT_ID'), os.getenv('ISP_SECRET_ID'), ORGANIZATION)
